0f1f791909df51c910b05cf9c011b20543fd3c82
maxibenner <maxibenner@gmail.com>
2023-04-15 18:03:09 +0200

    Update: Changed back to <img /> tag

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home({ images }: { images: any[] }) {
         <h1>Giulia B Nagle</h1>
         <div className={styles.imageContainer}>
           {images.map((image) => (
-            <Image
+            <img
               alt={image.title}
               key={image.title}
               className={styles.image}
