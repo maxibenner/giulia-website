2bc7043e666248d60083aee05920084fd845d635
maxibenner <maxibenner@gmail.com>
2023-04-15 18:18:35 +0200

    Update: Added revalidation

--- site/pages/api/revalidate.ts
+++ site/pages/api/revalidate.ts
@@ -10,12 +10,8 @@ export default async function handler(
     return res.status(401).json({ message: "Invalid token" });
   }
 
-  if (!req.body) {
-    return res.status(422).json({ message: "Invalid request body" });
-  }
-
   try {
-    await res.revalidate(path.join("/", req.body.data.slug));
+    await res.revalidate("/");
     return res.status(200).json({ revalidated: true });
   } catch (err) {
     return res.status(500).send("Error revalidating");
